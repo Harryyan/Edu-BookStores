2c8acc4fa69fe56e8a8a2f181c774221dac90c54
HarryYan <harryyan1238@gmail.com>
2020-12-25 07:52:41 +1300

    add new unit test

---
+++ WebApi_Course/WebAPI_UnitTest/SharedContext_Constructor .cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace WebAPI_UnitTest
+{
+    public class SharedContextConstructor : IDisposable
+    {
+        ITestOutputHelper _output;
+
+        public SharedContextConstructor(ITestOutputHelper output)
+        {
+            this._output = output;
+            _output.WriteLine("Execute constructor!");
+        }
+
+        #region Test case
+        [Fact(DisplayName = "SharedContext.Constructor.Case01")]
+        public void TestCase01()
+        {
+            _output.WriteLine("Execute case 01!");
+        }
+
+        [Fact(DisplayName = "SharedContext.Constructor.Case02")]
+        public void TestCase02()
+        {
+            _output.WriteLine("Execute case 02!");
+        }
+
+        [Fact(DisplayName = "SharedContext.Constructor.Case03")]
+        public void TestCase03()
+        {
+            _output.WriteLine("Execute case 03!");
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            _output.WriteLine("Execute dispose!");
+        }
+    }
+}
