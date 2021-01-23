245d33da30bbe506d8f7528ab88c8819797b2da8
HarryYan <harryyan1238@gmail.com>
2021-01-23 17:04:59 +1300

    add swagger

--- Project/BookStoresBackend/Startup.cs
+++ Project/BookStoresBackend/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace BookStoresBackend
 {
@@ -29,6 +31,28 @@ namespace BookStoresBackend
             services.AddMvc(option => option.EnableEndpointRouting = false)
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+            // Swagger : generate json endpoint
+            services.AddSwaggerGen(gen =>
+            {
+                gen.OperationFilter<AddResponseHeadersFilter>();
+                gen.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+
+                // add token to header, must have
+                gen.OperationFilter<SecurityRequirementsOperationFilter>();
+
+                // must be oauth2
+                gen.SwaggerDoc("v1.0", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Book Stores API Doc", Version = "v1.0" });
+                gen.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+            });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +67,13 @@ namespace BookStoresBackend
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(ui => ui.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Book Store API Doc"));
 
             app.UseEndpoints(endpoints =>
             {
