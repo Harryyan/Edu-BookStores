2ef60f1e683a7ddacde3b46d713cf4b29828e363
HarryYan <harryyan1238@gmail.com>
2021-01-24 08:52:26 +1300

    modify shared module

--- Project/BookStoresFrontend/src/app/shared/shared.module.ts
+++ Project/BookStoresFrontend/src/app/shared/shared.module.ts
@@ -1,12 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
+import { DropdownModule } from 'primeng/dropdown';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { ButtonModule } from 'primeng/button';
+import { FullCalendarModule } from 'primeng/fullcalendar';
+import { MessageModule } from 'primeng/message';
+import { TranslateModule } from '@ngx-translate/core';
 
+import { TopNavComponent } from '../core/components/top-nav/top-nav.component';
+import { LayoutComponent } from '../core/components/layouts/main/layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TopNavComponent,
+    LayoutComponent
+  ],
   imports: [
-    CommonModule
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    DropdownModule,
+    MenubarModule,
+    InputTextModule,
+    CalendarModule,
+    InputSwitchModule,
+    ButtonModule,
+    FullCalendarModule,
+    MessageModule,
+    TranslateModule    
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    MenubarModule,
+    InputTextModule,
+    CalendarModule,
+    InputSwitchModule,
+    ButtonModule,
+    FullCalendarModule,
+    MessageModule,
+    TranslateModule,
+    TopNavComponent,
+    LayoutComponent
   ]
 })
 export class SharedModule { }
