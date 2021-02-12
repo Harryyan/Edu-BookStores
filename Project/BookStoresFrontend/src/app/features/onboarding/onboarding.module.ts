515da193e92fa7e9142dbf35f0007b83fa98cb52
HarryYan <harryyan1238@gmail.com>
2021-02-13 08:56:22 +1300

    add onboarding

---
+++ Project/BookStoresFrontend/src/app/features/onboarding/onboarding.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './routing/login-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
