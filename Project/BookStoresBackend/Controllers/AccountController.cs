d59d7f1e0c89cd73bedf24b88340fc685938ab6a
HarryYan <harryyan1238@gmail.com>
2021-02-14 16:14:47 +1300

    add account controller

---
+++ Project/BookStoresBackend/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStores.Models;
+
+namespace BookStoresBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly BookStoresDBContext _context;
+
+        public AccountController(BookStoresDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User user)
+        {
+            var result = await _context.Users.FindAsync(user.EmailAddress);
+            return Ok();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
